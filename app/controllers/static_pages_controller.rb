6e25a63aaedfeb4a4a39254fe7f157f7b60e9671
helenemartin <helenemartin13@hotmail.com>
2017-01-24 20:33:51 +0000

    attempt at pulling event cover from Facebook event

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,10 @@ class StaticPagesController < ApplicationController
   def contact
   end
   def events
-    @graph = Koala::Facebook::API.new('EAACEdEose0cBAOCD9xFcZCR5VRL37KocE1ZCZBWSrb3EnYJJ6WABooW8oZBSEfITZC7SlZCvzqztfRlLHxRTEWdkH7idIUnKffyTc3QBW1qywVQ9hNVClEVaSMCUxWUGPWn8RNv8lOr147P8DDnWrs3qZCIxt3FKDYZBrNVX0oCh6QZDZD')
+    @graph = Koala::Facebook::API.new('EAACEdEose0cBALN29LTAxSJVZBSSxpPPSCfFHmDN44ratZBUbHWaUKyt0OJWivwO5vbAIr1gcj0YX1kDJdqjJvBhYX9aZAsu5Ci2rtjVfc6ubohOTt9O062ZBNZC0UL32G2lbZABs2IueGZAgFcZCNZAlVZCEFIsVhLa1h7xhqvnxZB5PBbBbcEVfnogaUCwep9ZCA4ZD')
+  #   @graph = Koala::Facebook::API.new('EAACEdEose0cBAOCD9xFcZCR5VRL37KocE1ZCZBWSrb3EnYJJ6WABooW8oZBSEfITZC7SlZCvzqztfRlLHxRTEWdkH7idIUnKffyTc3QBW1qywVQ9hNVClEVaSMCUxWUGPWn8RNv8lOr147P8DDnWrs3qZCIxt3FKDYZBrNVX0oCh6QZDZD')
     @events = @graph.get_connections('me', 'events?type=created')
-    @events = event.order(:date => :asc, :time => :asc) 
+    # @events = @graph.get_connections('me', '?fields=id,events{cover}')
+    # @events = event.order(:date => :asc, :time => :asc) 
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,9 +1,13 @@
 module StaticPagesHelper
 
 def cover_for_event(event)
-  @graph = Koala::Facebook::API.new('EAACEdEose0cBAOCD9xFcZCR5VRL37KocE1ZCZBWSrb3EnYJJ6WABooW8oZBSEfITZC7SlZCvzqztfRlLHxRTEWdkH7idIUnKffyTc3QBW1qywVQ9hNVClEVaSMCUxWUGPWn8RNv8lOr147P8DDnWrs3qZCIxt3FKDYZBrNVX0oCh6QZDZD')
-   @events = @graph.get_object( "#{event}?fields=cover")
-  @cover = @events["cover"]["source"]
-end
+  # @graph = Koala::Facebook::API.new('EAACEdEose0cBAOCD9xFcZCR5VRL37KocE1ZCZBWSrb3EnYJJ6WABooW8oZBSEfITZC7SlZCvzqztfRlLHxRTEWdkH7idIUnKffyTc3QBW1qywVQ9hNVClEVaSMCUxWUGPWn8RNv8lOr147P8DDnWrs3qZCIxt3FKDYZBrNVX0oCh6QZDZD')
+  @events = @graph.get_object("me", "?fields=events{cover}")
+  
+  
+  "IS THERE AN EVENT #{@events}"
+  @events ["cover"]["source_id"]
 
+
+end
 end
